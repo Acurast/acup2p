2692d07c1029d1b3364098c6c579b9a2aa6cf225
Julia Samól <j.samol@papers.ch>
2024-12-19 14:50:52 +0100

    chore(release): version 1.1.0-beta02

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 object Library {
     const val groupId = "com.acurast.p2p"
     const val artifactId = "acup2p"
-    const val version = "1.1.0-beta01"
+    const val version = "1.1.0-beta02"
 }
 
 android {
