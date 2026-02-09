58dfc718d49540de921c435f2d702f7591316989
Julia Samol <j.samol@papers.ch>
2026-02-09 13:30:03 +0100

    Release/1.2.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 object Library {
     const val groupId = "com.acurast.p2p"
     const val artifactId = "acup2p"
-    const val version = "1.2.1-beta01"
+    const val version = "1.2.1"
 }
 
 android {
